98b6667fd899a2fbfdd2904694c5d1f14d94cf64
Shek Rasel <swe.rasel@gmail.com>
2026-01-19 09:52:47 +0600

    changes for serial and add navbar

--- app/(office)/office/layout.tsx
+++ app/(office)/office/layout.tsx
@@ -1,37 +1,34 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarItems } from "@/types/types";
+import Navbar from "@/app/(office)/office/_components/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const sidebarItems: SidebarItems[] = [
     {
       title: "Dashboard",
-      url: "office/dashboard",
+      url: "/office/dashboard",
       icon: "/icons/dashboard-icon.png",
     },
     {
       title: "Budget Management",
-      url: "office/dashboard/budget-management",
+      url: "/office/dashboard/budget-management",
       icon: "",
     },
-    {
-      title: "Procurement",
-      url: "office/dashboard/procurement",
-      icon: "",
-    },
+    { title: "Procurement", url: "/office/dashboard/procurement", icon: "" },
     {
       title: "Order & Delivery",
-      url: "office/dashboard/order&delivery",
+      url: "/office/dashboard/order&delivery",
       icon: "",
     },
     {
       title: "Report & Audit",
-      url: "office/dashboard/report&audit",
+      url: "/office/dashboard/report&audit",
       icon: "",
     },
     {
       title: "Account Settings",
-      url: "office/dashboard/account-settings",
+      url: "/office/dashboard/account-settings",
       icon: "/icons/user-icon.png",
     },
   ];
@@ -39,10 +36,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar items={sidebarItems} />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+
+      <div className="min-h-screen w-full flex flex-col">
+        <header className="sticky top-0 z-50 w-full">
+          <Navbar />
+        </header>
+
+        <main className="flex-1 w-full bg-[#F8FAFC] p-3 md:p-4 lg:p-5">
+          {children}
+        </main>
+      </div>
     </SidebarProvider>
   );
 }
