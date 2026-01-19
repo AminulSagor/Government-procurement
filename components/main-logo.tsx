7f149a8ea9ade8e47382b0c13f255c48a2cdc64c
Shek Rasel <swe.rasel@gmail.com>
2026-01-19 09:55:25 +0600

    logo compo

---
+++ components/main-logo.tsx
@@ -0,0 +1,17 @@
+const Logo = () => {
+  return (
+    <svg
+      width="34"
+      height="26"
+      viewBox="0 0 34 26"
+      fill="none"
+      aria-hidden="true"
+    >
+      <ellipse cx="9" cy="13" rx="5.6" ry="10.5" fill="#fff" />
+      <ellipse cx="17" cy="13" rx="5.6" ry="10.5" fill="#fff" />
+      <ellipse cx="25" cy="13" rx="5.6" ry="10.5" fill="#fff" />
+    </svg>
+  );
+};
+
+export default Logo;
